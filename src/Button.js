c0c71e9f4b4aef8776f2532cbfd117ce5ca3d09d
Cristian Greco <cristianrgreco@gmail.com>
2020-04-25 17:13:10 +0100

    Extract Button and Link styles

---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+import styles from "./Button.module.css";
+
+export function Button({children, ...props}) {
+    return (
+        <div className={styles.Button} {...props}>
+            {children}
+        </div>
+    );
+}
--- src/MapControls.js
+++ src/MapControls.js
@@ -1,16 +1,18 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import styles from "./MapControls.module.css";
+import linkStyles from "./Link.module.css";
+import {Button} from "./Button";
 
 export function MapControls({isEmpty}) {
     return (
         <div className={styles.Controls}>
             {!isEmpty && (
                 <div className={styles.ControlItem}>
-                    <Link className={styles.Link} to="/">
-                        <div className={styles.Button} onClick={e => e.stopPropagation()}>
+                    <Link className={linkStyles.NoTextDecoration} to="/">
+                        <Button onClick={e => e.stopPropagation()}>
                             <span>New</span>
-                        </div>
+                        </Button>
                     </Link>
                 </div>
             )}
