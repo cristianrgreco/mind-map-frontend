5cee71ee7f993399bbccdadd2a3f61dc7f1a6bf0
Cristian Greco <cristianrgreco@gmail.com>
2020-04-26 16:56:08 +0100

    Add component test for Square

---
+++ src/Square.test.js
@@ -0,0 +1,32 @@
+import React from "react";
+import {render, unmountComponentAtNode} from "react-dom";
+import {act} from "react-dom/test-utils";
+import pretty from "pretty";
+import {Square} from "./Square";
+
+describe("Square", () => {
+  let container = null;
+
+  beforeEach(() => {
+    container = document.createElement("div");
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    unmountComponentAtNode(container);
+    container.remove();
+    container = null;
+  });
+
+  it("should match snapshot", () => {
+    act(() => {
+      render(<Square rect={{x: 0, y: 0, w: 10, h: 10}}/>, container);
+    });
+
+    expect(pretty(container.innerHTML)).toMatchInlineSnapshot(`
+      "<svg class=\\"Square\\">
+        <polyline points=\\"0,0 0,10 10,10 10,0 0,0\\" style=\\"fill: none; stroke: #ccc; stroke-width: 15;\\"></polyline>
+      </svg>"
+    `);
+  });
+});
